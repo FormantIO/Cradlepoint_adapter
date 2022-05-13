ab94e771695b97225de00bf3fd8f2aa71eadae93
tanim-islm <101572730+tanim-islm@users.noreply.github.com>
2022-05-13 13:02:46 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,16 @@
 # Ensure all prints make it to the journal log
 export PYTHONUNBUFFERED=true
 
+#!/usr/bin/env bash
+
+# Check for the setup lock
+FILENAME="setup.lock"
+ 
+if [ ! -f "$FILENAME" ]
+then
+  echo "$FILENAME not found - running setup..."
+  ./setup.sh
+fi 
+
 # Start the cradlepoint adapter
-bash -c "python3 -m pip install -r requirements.txt"
 bash -c "python3 cradlepoint_main.py"
